90e5679202f22476679df78fc9269a02b3cf3892
grechka91 <levor23@mail.ru>
2017-05-24 19:00:14 +0300

    Update upgrade_ocata_pike_containerized.sh

--- upgrade_ocata_pike_containerized.sh
+++ upgrade_ocata_pike_containerized.sh
@@ -23,8 +23,8 @@ function upgrade_undercloud_node
     sudo curl -L -o /etc/yum.repos.d/delorean.repo https://trunk.rdoproject.org/centos7-master/current-passed-ci/delorean.repo
     sudo curl -L -o /etc/yum.repos.d/delorean-current.repo https://trunk.rdoproject.org/centos7/current/delorean.repo
     sudo sed -i 's/\[delorean\]/\[delorean-current\]/' /etc/yum.repos.d/delorean-current.repo
-    sudo /bin/bash -c "cat <<EOF>>/etc/yum.repos.d/delorean-current.repo
-    includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,openstack-tripleo-common*,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements,openstack-puppet-modules,openstack-tripleo-ui,puppet-*
+    sudo /bin/bash -c "cat <<EOF>>/etc/yum.repos.d/delorean-current.repo \
+    includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,openstack-tripleo-common*,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements,openstack-puppet-modules,openstack-tripleo-ui,puppet-* \
     EOF"
     sudo curl -L -o /etc/yum.repos.d/delorean-deps.repo https://trunk.rdoproject.org/centos7/delorean-deps.repo
 
